3dd0b72edb99e7158d6e09782b42040fcdddea54
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2021-10-22 01:12:52 +0300

    Added custom GLCD font

---
+++ GLcdFont.h
@@ -0,0 +1,15 @@
+#ifndef UNTITLED_TEST_GLCDFONT_H
+#define UNTITLED_TEST_GLCDFONT_H
+
+#define FONT_HORIZ 0xF1
+#define FONT_VERT 0x1F
+
+
+class GLcdFont {
+public:
+    static unsigned char DefaultFont[1275 + 4];
+    static unsigned char CharacterFont[640 + 4];
+};
+
+
+#endif //UNTITLED_TEST_GLCDFONT_H
